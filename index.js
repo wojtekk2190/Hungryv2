b79f43df7348256ce1c411e337f27d23e6059fc8
wojtekk2190 <98337305+wojtekk2190@users.noreply.github.com>
2022-02-04 01:48:25 +0800

    Update index.js

--- index.js
+++ index.js
@@ -275,7 +275,7 @@ app.post("/webhook",express.json(),(request,response)=>{          //fulfillment 
       
       intentMap.set('Adding to order', addorder);
       
-      intentMap.set('Pick Up scheduling',pickup);
+      intentMap.set('Pick Up Scheduling',pickup);
 
       // 
       intentMap.set('Payment',Payment)
